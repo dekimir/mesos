24ca4047ca97ce0e465224f2045af32cf4cba9d2
Dejan Mircevski <github@mircevski.com>
2017-06-02 00:02:41 -0400

    Use a gen'd value in charsize.

--- ramfuzz/charsize.cpp
+++ ramfuzz/charsize.cpp
@@ -8,11 +8,8 @@ using namespace ramfuzz::runtime;
 
 int main(int argc, char* argv[])
 {
-#if 0
   gen g(argc, argv);
   const auto size = g.between<size_t>(0, 999999999);
-  cout << size << endl;
-#endif // 0
   mesos::TaskID t;
-  t.set_value(new char[1000], stoi(argv[1]));
+  t.set_value(new char[1000], /*stoi(argv[1])*/ size);
 }
